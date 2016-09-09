ad6007b033ca81ae4a4bfcd05453fffba6da1355
Krasiyan <hello@krasiyan.com>
2016-09-09 04:04:58 +0300

    emacs - added multiple-cursors package + key-bindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,6 +60,7 @@
      magit
      zoom-frm
      solarized-theme
+     multiple-cursors
      )))
 
 (condition-case nil
@@ -68,23 +69,9 @@
    (package-refresh-contents)
    (init--install-packages)))
 
+
+;; Setup key bindings
+(require 'key-bindings)
+
 ;; Set up appearance
 (require 'appearance)
-
-;; CUA mode because I'm a noob
-(cua-mode t)
-(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
-(transient-mark-mode 1) ;; No region when it is not highlighted
-(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
-
-;; Make shifted direction keys work on the Linux console or in an xterm
- (when (member (getenv "TERM") '("linux" "xterm" "xterm-256color"))
-  (dolist (prefix '("\eO" "\eO1;" "\e[1;"))
-    (dolist (m '(("2" . "S-") ("3" . "M-") ("4" . "S-M-") ("5" . "C-")
-                 ("6" . "S-C-") ("7" . "C-M-") ("8" . "S-C-M-")))
-      (dolist (k '(("A" . "<up>") ("B" . "<down>") ("C" . "<right>")
-                   ("D" . "<left>") ("H" . "<home>") ("F" . "<end>")))
-        (define-key function-key-map
-                    (concat prefix (cAr m) (car k))
-                    (read-kbd-macro ((cdr m) (cdr k))))))))
-
