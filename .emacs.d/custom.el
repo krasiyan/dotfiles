cca1295fd4d8a3e9e88640721a971ca770403e95
Krasiyan <hello@krasiyan.com>
2016-09-09 22:23:08 +0300

    emacs - cleanup custom file

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,15 +6,6 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
-;; '(ergoemacs-ctl-c-or-ctl-x-delay 0.2)
-;; '(ergoemacs-handle-ctl-c-or-ctl-x (quote both))
-;; '(ergoemacs-ini-mode t)
-;; '(ergoemacs-keyboard-layout "us")
-;; '(ergoemacs-mode nil)
-;; '(ergoemacs-smart-paste nil)
-;; '(ergoemacs-theme "standard")
-;; '(ergoemacs-theme-options nil)
-;; '(ergoemacs-use-menus t)
  '(initial-scratch-message nil)
  '(scroll-error-top-bottom nil)
  '(set-mark-command-repeat-pop nil)
