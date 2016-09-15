da8fa102d519435d8bb63597a2e6c4bf02ec84ff
Krasiyan <hello@krasiyan.com>
2016-09-16 01:35:54 +0300

    emacs - xml mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,6 +7,7 @@
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(initial-scratch-message nil)
+ '(nxml-slash-auto-complete-flag t)
  '(scroll-error-top-bottom nil)
  '(set-mark-command-repeat-pop nil)
  '(shift-select-mode t))
--- .emacs.d/settings/mode-mappings.el
+++ .emacs.d/settings/mode-mappings.el
@@ -5,10 +5,13 @@
 (add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
-
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode")
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+
+;; XML
+(autoload 'nxml-mode "nxml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 
 (provide 'mode-mappings)
