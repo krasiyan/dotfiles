73369e69a6099e81b13cb58426830a9e36e614b5
Krasiyan <hello@krasiyan.com>
2016-10-01 18:38:28 +0300

    emacs -git gutter tweaks

--- .emacs.d/settings/setup-git-gutter-fringe.el
+++ .emacs.d/settings/setup-git-gutter-fringe.el
@@ -1,11 +1,11 @@
 (require 'git-gutter-fringe)
 (global-git-gutter-mode +1)
+
+(custom-set-variables
+ '(git-gutter:update-interval 2)) ;; auto update
+
 (set-face-foreground 'git-gutter-fr:modified "yellow")
 (set-face-foreground 'git-gutter-fr:added    "blue")
-(set-face-foreground 'git-gutter-fr:deleted  "white")
-(custom-set-variables
- '(git-gutter:modified-sign "  ") ;; two space
- '(git-gutter:added-sign "++")    ;; multiple character is OK
- '(git-gutter:deleted-sign "--"))
+(set-face-foreground 'git-gutter-fr:deleted  "red")
 
 (provide 'setup-git-gutter-fringe)
