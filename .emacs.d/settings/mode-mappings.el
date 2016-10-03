4d8e6c7513c4d01038c907869182ea8fca5b39d4
Krasiyan <hello@krasiyan.com>
2016-10-03 21:36:04 +0300

    emacs - removed flycheck legacy

--- .emacs.d/settings/mode-mappings.el
+++ .emacs.d/settings/mode-mappings.el
@@ -3,7 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
-(add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode")
