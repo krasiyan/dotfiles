700335f63df431165b5fdcb54a7bfc46e3a9fc28
Krasiyan <hello@krasiyan.com>
2016-10-03 21:35:31 +0300

    emacs - flycheck use global eslint

--- .emacs.d/settings/setup-flycheck.el
+++ .emacs.d/settings/setup-flycheck.el
@@ -38,19 +38,6 @@ up before you execute another command."
   (flycheck-clear-idle-change-timer)
   (flycheck-buffer-automatically 'idle-change))
 
-;; find the locally installed eslint
-;; Kudos to http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
-(defun my/use-eslint-from-node-modules ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "node_modules"))
-         (eslint (and root
-                      (expand-file-name "node_modules/eslint/bin/eslint.js"
-                                        root))))
-    (when (and eslint (file-executable-p eslint))
-      (setq-local flycheck-javascript-eslint-executable eslint))))
-(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
-
 ;; disable the jshint default flycheck checker
 (setq-default flycheck-disabled-checkers
               (append flycheck-disabled-checkers
