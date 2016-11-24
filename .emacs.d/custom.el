3ea6e530440dd753f67848ce2c185a5709101507
Krasiyan <hello@krasiyan.com>
2016-11-25 00:27:11 +0200

    emacs modeline cleanup via the customizer :)

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,8 +6,20 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(display-time-default-load-average nil)
+ '(display-time-mail-directory nil)
+ '(display-time-mail-string "✉")
+ '(display-time-use-mail-icon t)
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(git-gutter:update-interval 2)
  '(initial-scratch-message nil)
  '(nxml-slash-auto-complete-flag t)
  '(scroll-error-top-bottom nil)
  '(set-mark-command-repeat-pop nil)
  '(shift-select-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
