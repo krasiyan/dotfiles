fe73e25b4f9a53ef544af9eaaa3f457179528b62
Krasiyan <hello@krasiyan.com>
2016-12-30 01:50:25 +0200

    i3 - visual improvements wip

--- scripts/keyboardlayouts.sh
+++ scripts/keyboardlayouts.sh
@@ -5,3 +5,5 @@ setxkbmap \
   -option grp:lalt_lshift_toggle \
   -option caps:backspace \
   -option terminate:ctrl_alt_bksp
+
+#localectl --no-convert set-x11-keymap us,bg pc105 ,bas_phonetic grp:lalt_lshift_toggle,caps:backspace,terminate:ctrl_alt_bksp
