d60eadd679fb267b1dd02392f1bc9e4006d7442c
Krasiyan <hello@krasiyan.com>
2017-01-08 02:39:55 +0200

    emacs key bindings - removed commented out code

--- .emacs.d/settings/key-bindings.el
+++ .emacs.d/settings/key-bindings.el
@@ -59,23 +59,6 @@
 ;; Page up for isearch-repeat-backward
 (define-key isearch-mode-map [prior] 'isearch-repeat-backward)
 
-;; C-w kill-this-buffer
-;; (defun ask-before-killing-buffer ()
-;;   (let ((buffer (current-buffer)))
-;;     (cond
-;;      ((and buffer-file-name (buffer-modified-p))
-;;       ;; If there's a file associated with the buffer,
-;;       ;; make sure it's saved
-;;       (y-or-n-p (format "Buffer %s modified; kill anyway? "
-;;                     (buffer-name))))
-;;      ((get-buffer-process buffer)
-;;       ;; If there's a process associated with the buffer,
-;;       ;; make sure it's dead
-;;       (y-or-n-p (format "Process %s active; kill anyway? "
-;;                     (process-name (get-buffer-process buffer)))))
-;;      (t t))))
-;; (add-to-list 'kill-buffer-query-functions
-;;              'ask-before-killing-buffer)
 (global-set-key "\C-w" 'kill-this-buffer)
 
 ;; Page Up/Down for cycling buffers
