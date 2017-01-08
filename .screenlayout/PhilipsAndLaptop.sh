f6f24d1418cdeef3dbec125c03d9297de53db336
Krasiyan <hello@krasiyan.com>
2017-01-08 02:03:42 +0200

    i3 - added the +shift+p shortcut to set my default monitor config

--- .screenlayout/PhilipsAndLaptop.sh
+++ .screenlayout/PhilipsAndLaptop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output eDP1 --mode 1600x900 --pos 0x180 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output DP2 --mode 1920x1080 --pos 3520x0 --rotate normal
-xfce4-panel -r
+#xfce4-panel -r
