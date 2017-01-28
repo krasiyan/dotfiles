13fbab06c73bc6228c60ec7e85b050872a32bf12
Krasiyan <hello@krasiyan.com>
2017-01-28 20:29:09 +0200

    changed caps lock to toggle the keyboard (instead of alt+shift)

--- scripts/keyboardlayouts.sh
+++ scripts/keyboardlayouts.sh
@@ -2,8 +2,7 @@ setxkbmap \
   -model "pc105" \
   -layout "us,bg" \
   -variant ",bas_phonetic" \
-  -option grp:lalt_lshift_toggle \
-  -option caps:backspace \
+  -option grp:caps_toggle \
   -option terminate:ctrl_alt_bksp
 
-#localectl --no-convert set-x11-keymap us,bg pc105 ,bas_phonetic grp:lalt_lshift_toggle,caps:backspace,terminate:ctrl_alt_bksp
+#localectl --no-convert set-x11-keymap us,bg pc105 ,bas_phonetic grp:caps_toggle,terminate:ctrl_alt_bksp
