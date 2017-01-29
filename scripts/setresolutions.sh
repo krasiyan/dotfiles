86313c54510e4b7ef205532d260dee4e67478b8f
Krasiyan <hello@krasiyan.com>
2017-01-29 16:54:39 +0200

    updated the setscreenresolutions script to add the reduced blanking fullhd resolution to both monitors

--- scripts/setresolutions.sh
+++ scripts/setresolutions.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xrandr --newmode "1920x1080R"  138.50  1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
+xrandr --newmode "1920x1080R" 138.50  1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
+xrandr --addmode HDMI1 "1920x1080R"
 xrandr --addmode DP2 "1920x1080R"
