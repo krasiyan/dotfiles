306d965f332712d0753d9c3bdae6e97d98f9cc15
Krasiyan <hello@krasiyan.com>
2017-01-29 16:55:20 +0200

    added new laptop screenlayout and updated the PhilipsAndLaptop one with the reduced blanking resolutions

--- .screenlayout/PhilipsAndLaptop.sh
+++ .screenlayout/PhilipsAndLaptop.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1600x900 --pos 0x180 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output DP2 --mode 1920x1080 --pos 3520x0 --rotate normal
-#xfce4-panel -r
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1600x900 --pos 0x180 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080R --pos 1600x0 --set "Broadcast RGB" "Full" --rotate normal --output DP2 --mode 1920x1080R --pos 3520x0 --rotate normal --set "Broadcast RGB" "Full"
