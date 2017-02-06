24b6b4300c2bc3e7133ac929d854472a64748fb5
Krasiyan <hello@krasiyan.com>
2017-02-07 01:30:10 +0200

    emacs2 - using js2-mode on typescript ('.ts') files

--- .emacs.d/settings/mode-mappings.el
+++ .emacs.d/settings/mode-mappings.el
@@ -1,6 +1,7 @@
 ;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.ts$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
 
