1066c30067fdf42b23b1b7d9701ed606727d30b6
Krasiyan <hello@krasiyan.com>
2017-09-10 17:58:06 +0300

    emacs - refactored the multi-term setup

--- .emacs.d/settings/setup-zsh.el
+++ .emacs.d/settings/setup-zsh.el
@@ -4,9 +4,12 @@
   "Start a terminal and rename buffer."
   (interactive)
   (multi-term)
-  (rename-buffer "term")
-  )
+  (rename-buffer "term"))
 
+(defun term-send-tab ()
+  "Send <tab> in term mode."
+  (interactive)
+  (term-send-raw-string "\t"))
 
 (global-set-key (kbd "M-z") 'zsh)
 (setq multi-term-program "/bin/zsh")
@@ -14,11 +17,17 @@
 (setq multi-term-scroll-show-maximum-output 1)
 (setq multi-term-scroll-to-bottom-on-output nil)
 (setq multi-term-switch-after-close nil)
-
 (add-hook 'term-mode-hook (lambda ()
+                            (add-to-list 'term-bind-key-alist '("<tab>" . term-send-tab))
+                            (add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
+                            (add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
+                            (setq term-buffer-maximum-size 10000)
+                            (global-company-mode 0)
+                            (company-quickhelp-mode 0)
+                            (company-mode 0)
+                            (setq yas-dont-activate t)
                             (setq show-trailing-whitespace nil)
                             (linum-mode 0)
-                            (highlight-indent-guides-mode 0)
                             (setq-local global-hl-line-mode nil)))
 
 (add-hook 'after-change-major-mode-hook
