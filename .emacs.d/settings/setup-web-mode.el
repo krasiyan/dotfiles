54bfc3782420f0dc0632e3eac74dcb9154bee946
Krasiyan <hello@krasiyan.com>
2017-10-01 18:34:51 +0300

    emacs - add forgotten 'setup-web-mode.el'

---
+++ .emacs.d/settings/setup-web-mode.el
@@ -0,0 +1,20 @@
+(require 'web-mode)
+
+(add-hook 'web-mode-hook  #'(lambda ()
+  (setq web-mode-markup-indent-offset 2)
+  (flycheck-mode 1)
+  (yas-activate-extra-mode 'html-mode)
+  (yas-activate-extra-mode 'js2-mode)))
+
+(setq
+  web-mode-code-indent-offset 2
+  web-mode-enable-auto-quoting nil
+  web-mode-markup-indent-offset 2
+  web-mode-css-indent-offset 2
+  web-mode-code-indent-offset 2
+  web-mode-attr-indent-offset 2
+  web-mode-script-padding 2
+  web-mode-enable-current-element-highlight t
+  web-mode-enable-current-column-highlight t)
+
+(provide 'setup-web-mode)
