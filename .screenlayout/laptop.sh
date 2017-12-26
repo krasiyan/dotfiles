84e6a1bcf2f5347eead4b1f0fc4ffad22ab1ae15
Krasiyan <hello@krasiyan.com>
2017-12-26 21:04:11 +0200

    upgrade the screen config for my new monitor configuration :tada:

--- .screenlayout/laptop.sh
+++ .screenlayout/laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL-1 --off --output eDP-1 --mode 1600x900 --pos 0x180 --rotate normal --primary --output DP1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP-2 --off --dpi 100
--- scripts/setresolutions.sh
+++ scripts/setresolutions.sh
@@ -2,3 +2,6 @@
 xrandr --newmode "1080pR" 138.50  1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
 xrandr --addmode HDMI-1 "1080pR"
 xrandr --addmode DP-2 "1080pR"
+
+xrandr --newmode "3840x2160_51.00_rb2"  442.22  3840 3848 3880 3920  2160 2198 2206 2212 +hsync -vsync
+xrandr --addmode DP-1 "3840x2160_51.00_rb2"
