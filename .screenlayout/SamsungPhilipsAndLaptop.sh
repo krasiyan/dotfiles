e9e3b7b967c7e0c0c97d23b173b75825853b6510
Krasiyan <hello@krasiyan.com>
2018-01-07 05:21:45 +0200

    downgrade the 4k monitor's resolution to mitigate the refresh rate cap / general system slowdown

--- .screenlayout/SamsungPhilipsAndLaptop.sh
+++ .screenlayout/SamsungPhilipsAndLaptop.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 3840x2160_51.00_rb2 --pos 1600x0 --rotate normal --output eDP-1 --mode 1600x900 --pos 0x1260 --rotate normal --output DP-2 --mode 1080pR --pos 5440x1080 --rotate normal --dpi 190
-i3-msg reload
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 3200x1800x59.98 --pos 1600x0 --rotate normal --output eDP-1 --mode 1600x900 --pos 0x900 --rotate normal --output DP-2 --mode 1080pR --pos 4800x720 --rotate normal --dpi 190
