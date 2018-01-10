3c10d3797c9c31eb1ef6cf0e38cd5f09335886b5
Krasiyan <hello@krasiyan.com>
2018-01-10 20:42:22 +0200

    minor dpi reduction

--- .screenlayout/SamsungPhilipsAndLaptop.sh
+++ .screenlayout/SamsungPhilipsAndLaptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 3200x1800x59.98 --pos 1600x0 --rotate normal --output eDP-1 --mode 1600x900 --pos 0x900 --rotate normal --output DP-2 --mode 1080pR --pos 4800x720 --rotate normal --dpi 190
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 3200x1800x59.98 --pos 1600x0 --rotate normal --output eDP-1 --mode 1600x900 --pos 0x900 --rotate normal --output DP-2 --mode 1080pR --pos 4800x720 --rotate normal --dpi 144
