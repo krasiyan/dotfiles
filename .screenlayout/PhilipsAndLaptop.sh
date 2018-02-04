cc9081c5f2be5154fc1da8010bd9d5c66043e764
Krasiyan Nedelchev <hello@krasiyan.com>
2018-02-05 00:28:41 +0200

    remove the obsolete screenlayouts

--- .screenlayout/PhilipsAndLaptop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL-1 --off --output eDP-1 --mode 1600x900 --pos 0x180 --rotate normal --output DP-1 --off --output HDMI-2 --off --output HDMI-1 --primary --mode 1080pR --pos 1600x0 --set "Broadcast RGB" "Full" --rotate normal --output DP-2 --mode 1080pR --pos 3520x0 --rotate normal --set "Broadcast RGB" "Full"
