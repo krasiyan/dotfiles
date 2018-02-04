0fcbf23c06374381144da26be811fab3a46be7ee
Krasiyan Nedelchev <hello@krasiyan.com>
2018-02-05 00:39:28 +0200

    upgrade the current screenlayout scripts to also set the 'xft' dpi and reload i3

--- .screenlayout/laptop.sh
+++ .screenlayout/laptop.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --off --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --dpi 192
+xrandr --output eDP-1-1 --off --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --dpi 192 && \
+    xfconf-query -c xsettings -p /Xft/DPI -s 192 && \
+    i3-msg restart
