ae0967afc2431c4688b165849d04202f3c136f45
Krasiyan Nedelchev <hello@krasiyan.com>
2018-02-05 00:35:22 +0200

    bump the 'laptopAndSamsung' screenlayout

--- .screenlayout/laptopAndSamsung.sh
+++ .screenlayout/laptopAndSamsung.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --dpi 192
+xrandr --output eDP-1-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --dpi 192
