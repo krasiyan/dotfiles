bdaa4bd1a4317bac110ae40df8cb1e98e6f87c04
Krasiyan Nedelchev <krasiyan.nedelchev@inradius.io>
2018-07-31 11:19:03 +0200

    emacs - fiplr ignore python specific cache/modules directories

--- .emacs.d/settings/setup-fiplr.el
+++ .emacs.d/settings/setup-fiplr.el
@@ -2,7 +2,7 @@
 
 (setq fiplr-root-markers '(".git"))
 (global-set-key (kbd "C-p") 'fiplr-find-file)
-(setq fiplr-ignored-globs '((directories (".git" "node_modules" "build" "public"))
+(setq fiplr-ignored-globs '((directories (".git" "node_modules" "build" "public" "__pycache__" "ENV"))
                         (files ("*.jpg" "*.png" "*.zip" "*~"))))
 
 (provide 'setup-fiplr)
