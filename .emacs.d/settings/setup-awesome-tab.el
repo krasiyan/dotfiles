fc121e3d9c743498b98d2dc3a3e726aa35d0ec67
Krasiyan Nedelchev <krasiyan.nedelchev@inradius.io>
2019-02-07 16:06:32 +0100

    emacs - wip: show the 'buffer modified' indicator in the tab names

--- .emacs.d/settings/setup-awesome-tab.el
+++ .emacs.d/settings/setup-awesome-tab.el
@@ -20,4 +20,27 @@
   :foreground "#657b83"
   :overline "#657b83")
 
+
+(defun awesome-tab-changed (tab)
+  "..."
+  (if (buffer-modified-p  (car tab))
+      "•"
+    ""))
+
+(defun awesome-tab-buffer-tab-label (tab)
+  "Return a label for TAB.
+That is, a string used to represent it on the tab bar."
+  (let ((label  (if awesome-tab--buffer-show-groups
+                    (format " [%s]%s" (awesome-tab-tab-tabset tab) (awesome-tab-changed tab))
+                  (format " %s%s" (awesome-tab-tab-value tab) (awesome-tab-changed tab)))))
+    ;; Unless the tab bar auto scrolls to keep the selected tab
+    ;; visible, shorten the tab label to keep as many tabs as possible
+    ;; in the visible area of the tab bar.
+    (if awesome-tab-auto-scroll-flag
+        label
+      (awesome-tab-shorten
+       label (max 1 (/ (window-width)
+                       (length (awesome-tab-view
+                                (awesome-tab-current-tabset)))))))))
+
 (provide 'setup-awesome-tab)
