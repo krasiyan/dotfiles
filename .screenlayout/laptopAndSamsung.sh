930dbf860b3afb31e26604f7fd0ded15851e455f
Krasiyan Nedelchev <hello@krasiyan.com>
2020-05-19 19:34:31 +0200

    i3/screenlayout - proper 4k 60hz external monitor setup
    - USB-C FTW!

--- .screenlayout/laptopAndSamsung.sh
+++ .screenlayout/laptopAndSamsung.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 3840x2160 --pos 0x2160 --rotate normal --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 0x0 --rotate normal -r 30 --output DP-5 --off --output DP-6 --off
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 3840x2160 --pos 0x2160 --rotate normal --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off --output DP-5 --off --output DP-6 --off
