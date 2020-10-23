128a5b76eaf9828cc27f4f968839d62de74d2ff0
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-23 03:23:38 +0200

    emacs - indentation higlighting - revert to characters as bitmaps don't support daemon mode

--- .emacs.d/settings/setup-highlight-indent-guides.el
+++ .emacs.d/settings/setup-highlight-indent-guides.el
@@ -1,7 +1,10 @@
 (require 'highlight-indent-guides)
 
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-(setq highlight-indent-guides-method 'bitmap)
-(setq highlight-indent-guides-bitmap-function 'highlight-indent-guides--bitmap-line)
+(setq highlight-indent-guides-method 'character)
+
+(set-face-background 'highlight-indent-guides-odd-face "darkgray")
+(set-face-background 'highlight-indent-guides-even-face "dimgray")
+(set-face-foreground 'highlight-indent-guides-character-face "dimgray")
 
 (provide 'setup-highlight-indent-guides)
