9a9de250ca5a2f520814355f8088cdbb678fbc02
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-23 03:12:38 +0200

    emacs - indentation higlighting improvements

--- .emacs.d/settings/setup-highlight-indent-guides.el
+++ .emacs.d/settings/setup-highlight-indent-guides.el
@@ -1,7 +1,8 @@
 (require 'highlight-indent-guides)
 
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-(setq highlight-indent-guides-method 'character)
-(setq highlight-indent-guides-character ?\|)
+(setq highlight-indent-guides-method 'bitmap)
+(setq highlight-indent-guides-bitmap-function 'highlight-indent-guides--bitmap-line)
+(setq highlight-indent-guides-responsive 'top)
 
 (provide 'setup-highlight-indent-guides)
