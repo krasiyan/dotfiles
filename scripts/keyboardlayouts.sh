6e7eadb2c3b6d5068a3d8483a8071685df62d962
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-25 04:55:10 +0100

    sway - remove the now unneeded compton,xkbdlayouts,xss-lock

--- scripts/keyboardlayouts.sh
+++
@@ -1,9 +0,0 @@
-setxkbmap \
-  -model "pc105" \
-  -layout "us,bg" \
-  -variant ",bas_phonetic" \
-  -option grp:caps_toggle \
-  -option terminate:ctrl_alt_bksp \
-  -option compose:ralt
-
-#localectl --no-convert set-x11-keymap us,bg pc105 ,bas_phonetic grp:caps_toggle,terminate:ctrl_alt_bksp
