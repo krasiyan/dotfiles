b826d809253087adfe8558572efaf425165e9110
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-25 05:02:55 +0100

    sway - use swaylock

--- scripts/locker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-xdotool key XF86AudioStop && \
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null || true && \
-xkblayout-state set 0 && \
-i3lock --color "#002b36" -n -e -f
