e1ab86031cd143a74f0e59009834f3e133224001
Krasiyan Nedelchev <hello@krasiyan.com>
2024-03-10 03:12:30 +0100

    chore: Xsecurelock eye candy

--- scripts/locker.sh
+++ scripts/locker.sh
@@ -3,4 +3,4 @@
 xdotool key XF86AudioStop && \
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null || true && \
 xkblayout-state set 0 && \
-xsecurelock
+XSECURELOCK_AUTH_BACKGROUND_COLOR=#002b36 XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_USERNAME=0 xsecurelock
