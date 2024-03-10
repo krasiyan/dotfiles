c0576c24f02a760c035e096e5d67ab1fc45255d5
Krasiyan Nedelchev <hello@krasiyan.com>
2024-03-10 02:38:28 +0100

    refactor: Migrate from i3lock to xsecurelock

--- scripts/locker.sh
+++ scripts/locker.sh
@@ -3,4 +3,4 @@
 xdotool key XF86AudioStop && \
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null || true && \
 xkblayout-state set 0 && \
-i3lock --color "#002b36" -n -e -f
+xsecurelock
