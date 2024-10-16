cf6113b73ef6f6f40a3535797a39fd4c580ae9e3
Krasiyan Nedelchev <hello@krasiyan.com>
2024-10-16 14:41:16 +0200

    feat(emacs): Use LSP for Python; misc LSP mode tweaks

--- .emacs.d/settings/setup-lsp-mode.el
+++ .emacs.d/settings/setup-lsp-mode.el
@@ -1,5 +1,7 @@
-(setq lsp-keymap-prefix "s-l")
 ;; (setq lsp-auto-configure nil)
+(setq lsp-keymap-prefix "s-l")
+(setq gc-cons-threshold 200000000)
+(setq read-process-output-max (* 10 (* 1024 1024)))
 (setq lsp-headerline-breadcrumb-enable nil)
 
 (add-to-list 'load-path (expand-file-name "lib/lsp-mode" user-emacs-directory))
--- .emacs.d/settings/setup-python-mode.el
+++ .emacs.d/settings/setup-python-mode.el
@@ -1,7 +1,19 @@
+(require 'company)
 (require 'add-python-venv-path)
+
+;; (add-hook 'go-mode-hook #'lsp-deferred)
 
 (add-hook 'python-mode-hook  #'(lambda ()
   (add-python-venv-path)
-  (flycheck-mode 1)))
+  (setq lsp-pylsp-plugins-ruff-enabled t)
+  (setq lsp-pylsp-plugins-mypy-enabled t)
+  (setq lsp-pylsp-plugins-mypy-live-mode t)
+  (setq lsp-pylsp-plugins-mypy-report-progress t)
+  (lsp-deferred)
+  (flycheck-mode 1)
+  (company-mode)
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t)
+  ))
 
 (provide 'setup-python-mode)
