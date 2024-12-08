c1df05e13f54b4a84583a6d4b393c7a7ba89fe44
Krasiyan Nedelchev <hello@krasiyan.com>
2024-12-08 22:45:56 +0100

    chore: Disable GitHub Copilot

--- .emacs.d/settings/setup-copilot.el
+++ .emacs.d/settings/setup-copilot.el
@@ -4,7 +4,7 @@
                    :branch "main"
                    :files ("dist" "*.el")))
 
-(add-hook 'prog-mode-hook 'copilot-mode)
+;; (add-hook 'prog-mode-hook 'copilot-mode)
 
 (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
 (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
