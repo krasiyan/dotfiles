d871951f7aa5f41e84b5e01095a8c262fe6be6c4
Krasiyan Nedelchev <hello@krasiyan.com>
2024-12-28 23:47:21 +0100

    chore(i3): Desktop setup improvements (locker && i3blocks)

--- scripts/locker.sh
+++ scripts/locker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-xdotool key XF86AudioStop && \
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null || true && \
-xkblayout-state set 0 && \
-XSECURELOCK_AUTH_BACKGROUND_COLOR=#002b36 XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_USERNAME=0 xsecurelock
+#xdotool key XF86AudioStop && \
+#xkblayout-state set 0 && \
+XSECURELOCK_BACKGROUND_COLOR=#002b36 XSECURELOCK_AUTH_BACKGROUND_COLOR=#002b36 XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_USERNAME=0 xsecurelock
